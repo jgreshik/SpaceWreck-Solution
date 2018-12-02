807f26851f57c9bbfea1197b6001ae642c8c8ac5
jgreshik <jgreshik@mines.edu>
2018-12-02 15:48:58 -0700

    added some helpful comments and explained input file format

--- maze_prjct_greshik.py
+++ maze_prjct_greshik.py
@@ -1,4 +1,4 @@
-# CSCI406 MadMaze Spacewreck Maze Problem
+# MadMaze Spacewreck Maze Problem
 # Author: Joseph Greshik 
 # 
 #	This script reads in a formatted maze blueprint and 
@@ -8,21 +8,39 @@
 # 	has a solution. If a solution is found, the script
 # 	outputs to standard output the step-by-step solution
 #	The initial and condensed graphs are visualized
+#
+#   Maze blueprint is structured as follows:
+#       line 1 contains two integers, n and m
+#       Line 2 contains n-1 chars where the ith char represents the color of 
+#           the ith vertex. The vertex with index n is the exit and has no color
+#       Line 3 contains two integers s1 and s2, representing the index of the 
+#           starting vertices of Captain Rocket and Lieutenant Lucky, respectively
+#       each of the next m lines contains two integers a and b and one char c,
+#           in that order, representing a corridor with color c from a to b
+#           note that a, b are within 1 to n
+#       
 
 import networkx as nx
 import matplotlib.pyplot as plt
 from collections import namedtuple
 import string
 
+######## CODE FOR SOLUTION ########
+
 # State tuple is how nodes are stored in RUG. 
+# State contains locations of Rocket (R) and
+# Lucky (L) as integers corresponding to nodes
+# of original graph
 # Need for:
 #	creating RUG nodes
 #	declaring source in bfs of RUG
 #	checking items in bfs tree
-State=namedtuple("State","L R")
+State=namedtuple("State","R L")
 
 #### makeGraph:
-####	takes file of defined structure and returns directed graph & final location 
+####	takes file of defined structure and returns directed graph & final location
+####    we will output locations as A=1, B=2,...Z=26,AA=27
+####    but for ease of indexing we will use A=0, B=1,... in the graph nodes 
 def makeGraph(file_name):
 	G=nx.DiGraph()
 	file=open(file_name)
@@ -41,6 +59,81 @@ def makeGraph(file_name):
 		line=file.readline().replace("\n", "").replace("\r", "").split(" ")
 		G.add_edge(int(line[0])-1,int(line[1])-1,color=str(line[2]))
 	return G,solnLoc
+
+#### refurbish:
+#### 	convert old_graph into DAG consisting of directed state nodes
+####    states represent Rocket and Lucky's location on the game board
+####    states are represented as State named tuple where 
+####    astronaut locations are denoted as  0=A, 1=B, ..., 26=AA, 27=end
+####    Possible states (succeeding current state) are added iff Rocket 
+####    or Lucky can move to the state depending on each other's current state
+def refurbish(old_graph,solLoc,loc_Rocket,loc_Lucky):
+    G=nx.DiGraph()
+    check_points=[[-1 for i in range(solLoc+1)] for j in range(solLoc+1)]
+    
+    def check(state,next_node,isL):
+        if isL:
+            if old_graph.edges()[(state[0],next_node)]['color']==old_graph.nodes()[state[1]]['color']:
+                next_state=State(next_node,state[1])
+                G.add_node(next_state)
+                if check_points[next_state[0]][next_state[1]]==-1: G.add_edge(state,next_state)
+                refurb(next_state)
+        else:
+            if old_graph.edges()[(state[1],next_node)]['color']==old_graph.nodes()[state[0]]['color']:
+                next_state=State(state[0],next_node)
+                G.add_node(next_state)
+                if check_points[next_state[0]][next_state[1]]==-1: G.add_edge(state,next_state)
+                refurb(next_state)
+            
+    
+    def refurb(state):
+        if check_points[state[0]][state[1]]!=-1: return
+        check_points[state[0]][state[1]]=state
+        for next_left in list(old_graph.adj.items())[state[0]][1]: check(state,next_left,True)  
+        for next_right in list(old_graph.adj.items())[state[1]][1]: check(state,next_right,False)
+    
+    starting_state=State(loc_Rocket,loc_Lucky)
+    refurb(starting_state)
+    
+    return G
+
+#### print_node_alpha:
+#### 	returns alpha string of node for ease of reading when 
+####   outputting final path
+def print_node_alpha(num,s):
+	out=""
+	if num==s+1: return 'end'
+	for i in range(int((num-1)/26)+1):
+	    out=out+list(string.ascii_uppercase)[(num-1)%26]
+	    num=num-26
+	return out
+
+#### get_diff:
+#### 	return who moved in bfs parent-child relationship
+def get_diff(state_list,s):
+    if state_list[0][0]==state_list[1][0]: return('L '+str(state_list[0][1]+1)+' # Lucky moves to '+print_node_alpha(state_list[0][1]+1,s))
+    else: return('R '+str(state_list[0][0]+1)+' # Rocket moves to '+print_node_alpha(state_list[0][0]+1,s))
+
+#### traverse_bfs_tree:
+####	traverse tree made by reduced_graph bfs according to networkx bfs predecessor fn
+def traverse_bfs_tree(tree,solnLoc):
+    finished=False
+    out=[]
+    for i in range(len(tree)):
+        if finished==False and tree[-i-1][0][0] or tree[-i-1][0][1]==solnLoc: 
+            finished=True
+            out.append(get_diff(tree[-i-1],solnLoc))
+            index=i
+        if finished==True and tree[-i-1][0]==tree[-index-1][1]:
+            index=i
+            out.append(get_diff(tree[-i-1],solnLoc))
+    if finished:
+        for i in range(len(out)):
+            print(out[-i-1])
+    else:
+        print('No solution')
+        
+######## VISUALIZATION CODE ########
 
 #### drawVanillaGraph: 
 ####	draw full graph with colored, numbered nodes and colored, directed edges
@@ -86,43 +179,8 @@ def drawVanillaGraph(graph, pos):
     plt.axis('off')
     plt.show()
 
-#### refurbish:
-#### 	convert old_graph into DAG consisting of directed state nodes
-####    states represent Rocket and Lucky's location on the game board
-####    states are represented as State named tuple where 
-####    astronaut locations are denoted as  0=A, 1=B, ..., 26=AA, 27=end
-def refurbish(old_graph,solLoc,loc_Lucky,loc_Rocket):
-    G=nx.DiGraph()
-    check_points=[[-1 for i in range(solLoc+1)] for j in range(solLoc+1)]
-    
-    def check(state,next_node,isL):
-        if isL:
-            if old_graph.edges()[(state[0],next_node)]['color']==old_graph.nodes()[state[1]]['color']:
-                next_state=State(next_node,state[1])
-                G.add_node(next_state)
-                if check_points[next_state[0]][next_state[1]]==-1: G.add_edge(state,next_state)
-                refurb(next_state)
-        else:
-            if old_graph.edges()[(state[1],next_node)]['color']==old_graph.nodes()[state[0]]['color']:
-                next_state=State(state[0],next_node)
-                G.add_node(next_state)
-                if check_points[next_state[0]][next_state[1]]==-1: G.add_edge(state,next_state)
-                refurb(next_state)
-            
-    
-    def refurb(state):
-        if check_points[state[0]][state[1]]!=-1: return
-        check_points[state[0]][state[1]]=state
-        for next_left in list(old_graph.adj.items())[state[0]][1]: check(state,next_left,True)  
-        for next_right in list(old_graph.adj.items())[state[1]][1]: check(state,next_right,False)
-    
-    starting_state=State(loc_Lucky,loc_Rocket)
-    refurb(starting_state)
-    
-    return G
-
 #### drawRefurbishedGraph:
-####	draw simpler refurbished graph
+####	draw refurbished graph of character states
 def drawRefurbishedGraph(graph, pos):
     # size of plot
     plt.figure(figsize=(10,10),dpi=100)
@@ -149,55 +207,27 @@ def drawRefurbishedGraph(graph, pos):
     plt.axis('off')
     plt.show()
 
-#### print_node_alpha:
-#### 	returns alpha string of node for ease of reading
-def print_node_alpha(num,s):
-	out=""
-	if num==s+1: return 'end'
-	for i in range(int((num-1)/26)+1):
-	    out=out+list(string.ascii_uppercase)[(num-1)%26]
-	    num=num-26
-	return out
 
-#### get_diff:
-#### 	return who moved in bfs parent-child relationship
-def get_diff(state_list,s):
-    if state_list[0][0]==state_list[1][0]: return('L '+str(state_list[0][1]+1)+' # Lucky moves to '+print_node_alpha(state_list[0][1]+1,s))
-    else: return('R '+str(state_list[0][0]+1)+' # Rocket moves to '+print_node_alpha(state_list[0][0]+1,s))
-
-#### traverse_bfs_tree:
-####	traverse tree made by reduced_graph bfs according to networkx bfs predecessor fn
-def traverse_bfs_tree(tree,solnLoc):
-    finished=False
-    out=[]
-    for i in range(len(tree)):
-        if finished==False and tree[-i-1][0][0] or tree[-i-1][0][1]==solnLoc: 
-            finished=True
-            out.append(get_diff(tree[-i-1],solnLoc))
-            index=i
-        if finished==True and tree[-i-1][0]==tree[-index-1][1]:
-            index=i
-            out.append(get_diff(tree[-i-1],solnLoc))
-    for i in range(len(out)):
-        print(out[-i-1])
-
+######## MAIN FUNCTION ########
+        
 #### main:
 ####	this is where all script happenings happen
 def main():
-	# calling graph 'corr'
-	# also keeping track of where the solution location should be in the graph
-	(corr,solution_location)=makeGraph("input.txt") 
-	#draw our graph 'corr'
-	drawVanillaGraph(corr,pos=nx.kamada_kawai_layout(corr))
-	# refurbish graph to better fit our needs
-	# our needs are to find if a path exists to 'end' from the starting location
-	# making new graph R(efurbished) U(ltimate) G(raph)
-	RUG=refurbish(corr,solution_location,0,1)
-	drawRefurbishedGraph(RUG,pos=nx.fruchterman_reingold_layout(RUG))
-	# now print a bfs traversal of the refurbished graph
-	# bfs predecessors and parents stored in y
-	y=list(nx.bfs_predecessors(RUG,State(L=0,R=1)))
-	# now traverse bfs tree
-	traverse_bfs_tree(y,solution_location)
+    # create graph from problem statement input
+    # keeping track of where the solution location should be in the graph
+    (original_graph,solution_location)=makeGraph("input.txt") 
+    #draw our graph 'corr'
+    drawVanillaGraph(original_graph,pos=nx.kamada_kawai_layout(original_graph))
+    # refurbish graph to better fit our needs
+    # our needs are to find if a path exists to 'end' from the starting location
+    # making new graph R(efurbished) U(ltimate) G(raph)
+    RUG=refurbish(original_graph,solution_location,0,1)
+    drawRefurbishedGraph(RUG,pos=nx.fruchterman_reingold_layout(RUG))
+    # now print a bfs traversal of the refurbished graph
+    # bfs predecessors and parents stored in y 
+    # Rocket starts at 0 (A) and Lucky starts at 1 (B)
+    y=list(nx.bfs_predecessors(RUG,State(R=0,L=1))) 
+    # now traverse bfs tree and output solution (or lack thereof)
+    traverse_bfs_tree(y,solution_location)
 
 main()
